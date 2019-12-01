6b7981611cdf45c73efce7d5b7eba117c3acce73
James <james.duong93@gmail.com>
2019-12-02 01:05:11 +1100

    renaming

--- src/config/s3.ts
+++ src/config/s3.ts
@@ -1,6 +1,6 @@
 export const S3Config = {
   bucketName: 'phongspho',
-  dirName: 'meal_images',
+  dirName: 'images',
   region: 'ap-southeast-2',
   accessKeyId: process.env.REACT_APP_S3_ACCESS_KEY,
   secretAccessKey: process.env.REACT_APP_S3_SECRET_KEY
